4bc03b7bb20dea75fb6c6afba8516ccc1eafb8f7
MENDOZA2414 <jmendoza_21@alu.uabcs.mx>
2025-05-20 21:46:25 -0700

    Hover en checbox

--- src/shared/components/TableCheckbox.jsx
+++ src/shared/components/TableCheckbox.jsx
@@ -12,8 +12,9 @@ const TableCheckbox = ({ defaultChecked = false, onChange }) => {
   return (
     <button
       onClick={toggle}
-      className={`w-[32px] h-[32px] rounded-md flex items-center justify-center transition duration-150 ease-in-out shadow-sm
-        ${checked ? 'bg-blue-500 border-0' : 'bg-white border-2 border-gray-400'}`}
+      className={`w-[32px] h-[32px] rounded-md flex items-center justify-center
+        ${checked ? 'bg-blue-500 border-0' : 'bg-white border-2 border-gray-400'}
+        hover:brightness-90 transition duration-150 ease-in-out`}
     >
       {checked && (
         <svg
